c55f85ee6894c57f039247d5db4318d04b5c7fc3
liyang <15238376457@163.com>
2022-03-05 17:55:36 +0800

    update ios

--- ios/Classes/SwiftWoshilllFlutterPlugin.swift
+++ ios/Classes/SwiftWoshilllFlutterPlugin.swift
@@ -2,6 +2,7 @@ import Flutter
 import UIKit
 
 public class SwiftWoshilllFlutterPlugin: NSObject, FlutterPlugin {
+  var configMap = [String:AnyObject]()
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "woshilll_flutter_plugin", binaryMessenger: registrar.messenger())
     let instance = SwiftWoshilllFlutterPlugin()
@@ -9,6 +10,13 @@ public class SwiftWoshilllFlutterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if (call.method == "setBrightness") {
+        UIScreen.main.brightness = CGFloat(call.arguments as! Double)
+    } else if (call.method == "getBrightness") {
+        result(UIScreen.main.brightness)
+    }else if (call.method == "setConfig") {
+        let map = (call.arguments! as! [String:Any])
+        self.configMap[map["key"] as! String] = map["value"] as AnyObject?
+    }
   }
 }
